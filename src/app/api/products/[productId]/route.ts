9a560d7080bcb39e452da5cd68a777175f70d3e7
Mathias Oxholm <mo.micheelsen@gmail.com>
2025-09-07 18:29:21 +0200

    Update route param type

--- src/app/api/products/[productId]/route.ts
+++ src/app/api/products/[productId]/route.ts
@@ -2,7 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 import Products from "@/db/products.json";
 
-export async function GET(_request: NextRequest, { params }: PageProps<'/products/[productId]'>) {
+type RouteProps = {
+  params: Promise<{ productId: string }>;
+}
+
+export async function GET(_request: NextRequest, { params }: RouteProps) {
   const { productId } = (await params);
 
   const product = Products.find((product) => product.id === productId);
